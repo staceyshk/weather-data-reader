d83865d09f91e0d7cafae8b215fce42dccef57b3
Stacey Shkuratoff <staceyshk@gmail.com>
2021-08-03 20:25:20 -0700

    Added util classes and tests for reading a csv string and calculating the spread

---
+++ src/Utils.js
@@ -0,0 +1,71 @@
+/**
+ * Assuming the spread between temperatures is the difference.
+ * Added the absolute of it in case the max/min are switched because
+ * the spread is just the amount between anyway
+ * @param {object} dayInfo 
+ * @returns difference between high temp and low temp
+ */
+export const spread  = (dayInfo) => {
+  return Math.abs(dayInfo.maxTemp - dayInfo.minTemp)
+}
+
+/**
+ * Takes the array of days with max and min temp and returns the lowest day/days 
+ * @param {array} dayInfoList
+ * @returns {array} day numbers with lowest spreads
+ */
+export const lowestSpreadDay = (dayInfoList) => {
+  // Easy exit, no days to sort
+  if (!dayInfoList || dayInfoList.length === 0) return []
+
+  // Add the spread to each object
+  dayInfoList.forEach(dayInfo => dayInfo.spread = spread(dayInfo))
+
+  // Sort by the lowest spread
+  const sortedDays = dayInfoList.sort((a, b) => (a.spread >= b.spread) ? 1 : -1)
+
+  // This wasn't in the requirement, but if two days are equal I'd like to return them both
+  const shortestSpreadDays = sortedDays.filter(spreadDayInfo => spreadDayInfo.spread === sortedDays[0].spread)
+  return shortestSpreadDays.map(shortestSpreadDay => shortestSpreadDay.day)
+}
+
+/**
+ * Reads in csv, trims lines and returns weather data
+ * 
+ * @param {string} csvString 
+ * @returns {array} partial weather data in object format that can be used in
+ */
+export const readCSV = (csvString) => {
+  const lines = csvString.split('\n')
+  // Trim the line end off and split into parts
+  const headers = lines[0]
+  if (headers !== 'Day,MxT,MnT,AvT,AvDP,1HrP TPcpn,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP') {
+    throw new Error('Wrong CSV format')
+  }
+
+  if (lines.length < 2){
+    throw new Error('Missing weather data')
+  }
+ 
+  const weatherData = []
+
+  // Each weather data has day,max,min temps in that order
+  for (let i = 1; i < lines.length; i++) {
+    const weatherDataItem = lines[i].replace('\n', '').split(',')
+    const day = parseInt(weatherDataItem[0])
+    const maxTemp = parseInt(weatherDataItem[1])
+    const minTemp = parseInt(weatherDataItem[2])
+
+    if (isNaN(day) || isNaN(maxTemp) || isNaN(minTemp)) {
+      throw new Error('Invalid weather data on line ' + i)
+    }
+
+    weatherData.push({
+      day: day,
+      maxTemp: maxTemp,
+      minTemp: minTemp
+    })
+  }
+
+  return weatherData
+}
---
+++ src/Utils.test.js
@@ -0,0 +1,66 @@
+import { readCSV, spread, lowestSpreadDay } from './Utils'
+
+test('Calculates temperature spread', () => {
+  const dayInfo = { day: 1, minTemp: 3, maxTemp: 6 }
+  expect(spread(dayInfo)).toEqual(3)
+})
+
+test('Returns lowest spread in array of day objects', () => {
+  const dayInfoArray = [
+    { day: 1, minTemp: 3, maxTemp: 6 },
+    { day: 2, minTemp: 6, maxTemp: 6 },
+    { day: 3, minTemp: 5, maxTemp: 6 },
+  ]
+
+  expect(lowestSpreadDay(dayInfoArray)).toEqual([2])
+})
+
+test('Returns multiple lowest spreads in array of day objects', () => {
+  const dayInfoArray = [
+    { day: 1, minTemp: 3, maxTemp: 6 },
+    { day: 2, minTemp: 2, maxTemp: 6 },
+    { day: 3, minTemp: 5, maxTemp: 6 },
+    { day: 4, minTemp: 5, maxTemp: 6 },
+  ]
+
+  expect(lowestSpreadDay(dayInfoArray)).toEqual([3,4])
+})
+
+test('Returns empty array when no days are passed', () => {
+  expect(lowestSpreadDay(null)).toEqual([]);
+})
+
+test('Reads CSV Lines and returns object for weather data', () => {
+  const testCsvString = 'Day,MxT,MnT,AvT,AvDP,1HrP TPcpn,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP\n1,88,59,74,53.8,0,280,9.6,270,17,1.6,93,23,1004.5\n2,79,63,71,46.5,0,330,8.7,340,23,3.3,70,28,1004.5'
+  const weatherData = readCSV(testCsvString)
+  expect(weatherData).toEqual([{
+    day: 1,
+    maxTemp: 88,
+    minTemp: 59
+  },{
+    day: 2,
+    maxTemp: 79,
+    minTemp: 63
+  }])
+})
+
+test('Throws exception for wrong header format', () => {
+  const testCsvString = 'Day,MxT,MnT,AvT,AvDP,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP\n1,88,59,74,53.8,0,280,9.6,270,17,1.6,93,23,1004.5\n2,79,63,71,46.5,0,330,8.7,340,23,3.3,70,28,1004.5'
+  expect(() => { readCSV(testCsvString) }).toThrow('Wrong CSV format')
+})
+
+test('Throws exception for not enough lines of data', () => {
+  const testCsvString = 'Day,MxT,MnT,AvT,AvDP,1HrP TPcpn,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP'
+  expect(() => { readCSV(testCsvString) }).toThrow('Missing weather data')
+})
+
+test('Throws exception if weather data is not a number on line 1', () => {
+  const testCsvString = 'Day,MxT,MnT,AvT,AvDP,1HrP TPcpn,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP\n1,S,TT,74,53.8,0,280,9.6,270,17,1.6,93,23,1004.5\n2,79,63,71,46.5,0,330,8.7,340,23,3.3,70,28,1004.5'
+  expect(() => { readCSV(testCsvString) }).toThrow('Invalid weather data on line 1')
+})
+
+
+test('Throws exception if weather data is not a number on line 2', () => {
+  const testCsvString = 'Day,MxT,MnT,AvT,AvDP,1HrP TPcpn,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP\n1,88,59,74,53.8,0,280,9.6,270,17,1.6,93,23,1004.5\n2,TT,63,71,46.5,0,330,8.7,340,23,3.3,70,28,1004.5'
+  expect(() => { readCSV(testCsvString) }).toThrow('Invalid weather data on line 2')
+})
